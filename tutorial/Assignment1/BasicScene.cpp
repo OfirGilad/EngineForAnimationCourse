3468c0c896b918362571e2140adc3dde16b449bc
Ofir Gilad <ofir.gilad9631@gmail.com>
2022-11-15 00:52:35 +0200

    basic viewer works

--- tutorial/Assignment1/BasicScene.cpp
+++ tutorial/Assignment1/BasicScene.cpp
@@ -6,6 +6,19 @@
 #include "igl/read_triangle_mesh.cpp"
 #include "igl/edge_flaps.h"
 
+#include <igl/circulation.h>
+#include <igl/collapse_edge.h>
+#include <igl/edge_flaps.h>
+#include <igl/decimate.h>
+#include <igl/shortest_edge_and_midpoint.h>
+#include <igl/parallel_for.h>
+#include <igl/read_triangle_mesh.h>
+#include <igl/opengl/glfw/Viewer.h>
+#include <Eigen/Core>
+#include <iostream>
+#include <set>
+
+
 // #include "AutoMorphingModel.h"
 
 using namespace cg3d;
@@ -69,6 +82,101 @@ void BasicScene::Init(float fov, int width, int height, float near, float far)
     std::cout<< "faces to edges: \n "<< EMAP.transpose()<<std::endl;
     std::cout<< "edges indices: \n" << EI.transpose() <<std::endl;
 
+    // New Code - Start
+
+    using namespace std;
+    using namespace Eigen;
+    using namespace igl;
+
+    igl::opengl::glfw::Viewer viewer;
+    igl::min_heap< std::tuple<double, int, int> > Q;
+
+    const auto& reset = [&]()
+    {
+        edge_flaps(F, E, EMAP, EF, EI);
+        C.resize(E.rows(), V.cols());
+        VectorXd costs(E.rows());
+        // https://stackoverflow.com/questions/2852140/priority-queue-clear-method
+        // Q.clear();
+        Q = {};
+        EQ = Eigen::VectorXi::Zero(E.rows());
+        {
+            Eigen::VectorXd costs(E.rows());
+            igl::parallel_for(E.rows(), [&](const int e)
+                {
+                    double cost = e;
+            RowVectorXd p(1, 3);
+            shortest_edge_and_midpoint(e, V, F, E, EMAP, EF, EI, cost, p);
+            C.row(e) = p;
+            costs(e) = cost;
+                }, 10000);
+            for (int e = 0;e < E.rows();e++)
+            {
+                Q.emplace(costs(e), e, 0);
+            }
+        }
+
+        num_collapsed = 0;
+        viewer.data().clear();
+        viewer.data().set_mesh(V, F);
+        viewer.data().set_face_based(true);
+    };
+
+    const auto& pre_draw = [&](igl::opengl::glfw::Viewer& viewer)->bool
+    {
+        // If animating then collapse 10% of edges
+        if (viewer.core().is_animating && !Q.empty())
+        {
+            bool something_collapsed = false;
+            // collapse edge
+            const int max_iter = std::ceil(0.01 * Q.size());
+            for (int j = 0;j < max_iter;j++)
+            {
+                if (!collapse_edge(shortest_edge_and_midpoint, V, F, E, EMAP, EF, EI, Q, EQ, C))
+                {
+                    break;
+                }
+                something_collapsed = true;
+                num_collapsed++;
+            }
+
+            if (something_collapsed)
+            {
+                viewer.data().clear();
+                viewer.data().set_mesh(V, F);
+                viewer.data().set_face_based(true);
+                viewer.core().is_animating = false;
+            }
+        }
+        return false;
+    };
+
+    const auto& key_down =
+        [&](igl::opengl::glfw::Viewer& viewer, unsigned char key, int mod)->bool
+    {
+        switch (key)
+        {
+        case ' ':
+            viewer.core().is_animating ^= 1;
+            break;
+        case 'R':
+        case 'r':
+            reset();
+            break;
+        default:
+            return false;
+        }
+        return true;
+    };
+
+    reset();
+    viewer.core().background_color.setConstant(1);
+    viewer.core().is_animating = false;
+    viewer.callback_key_down = key_down;
+    viewer.callback_pre_draw = pre_draw;
+    viewer.launch();
+
+    // New Code - End
 }
 
 void BasicScene::Update(const Program& program, const Eigen::Matrix4f& proj, const Eigen::Matrix4f& view, const Eigen::Matrix4f& model)
